9f21670855e3f006467dafaafd7981709cd0ddc0
ahmadaimandev <aimanzulbahari@gmail.com>
2025-08-17 15:55:58 +0800

    add changes

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import 'aos/dist/aos.css'; // You can also use <link> for styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+
 // CSS
 import '/assets/css/main.css';
 import '/assets/css/scrollbar.css';
@@ -17,6 +18,7 @@ import '/assets/css/skill-card.css';
 import '/assets/css/floating-heart.css';
 import '/assets/css/achivement.css';
 import '/assets/css/statistic.css';
+import '/assets/css/ribbon.css';
 
 
 
